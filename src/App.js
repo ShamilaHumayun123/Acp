ba4f53839495fd0c51103810f48325125d9d2c29
Shamila Humayun <shamilahumayun942@gmail.com>
2024-09-12 08:50:14 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my First Page.
+        </p>
+       <Greet></Greet> 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Greet.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function Greet(){
+return <p>This is first component and first paragraph</p>
+}
+export default Greet;
